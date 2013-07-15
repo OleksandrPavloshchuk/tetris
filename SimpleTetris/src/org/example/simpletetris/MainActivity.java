0726c64dfd9e42324202a32cf882a6f7a6b565e2
OleksandrPavloshchuk <ap730518@gmail.com>
2013-07-15 17:17:05 +0300

    + screen touch listener

--- SimpleTetris/src/org/example/simpletetris/MainActivity.java
+++ SimpleTetris/src/org/example/simpletetris/MainActivity.java
@@ -2,12 +2,45 @@ package org.example.simpletetris;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private TetrisView tetrisView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		tetrisView = TetrisView.class.cast(findViewById(R.id.tetris));
+		
+		tetrisView.setOnTouchListener( new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				int direction = getDirection(v, event);
+				String text = String.format("Direction: %d", direction );
+				Toast toast = Toast.makeText( getApplicationContext(), text, Toast.LENGTH_SHORT );
+				toast.show();
+				return true;
+			}
+		});
+	}
+	
+	private int getDirection(View v, MotionEvent event) {
+        // Normalize x,y between 0 and 1
+        float x = event.getX() / v.getWidth();
+        float y = event.getY() / v.getHeight();
+
+        // Direction will be [0,1,2,3] depending on quadrant
+        int direction = 0;
+        direction = (x > y) ? 1 : 0;
+        direction |= (x > 1 - y) ? 2 : 0;
+        
+        // 0 - left, 1 - rotate, 2 - down, 3 - right
+        
+        return direction;
 	}
 }
--- SimpleTetris/src/org/example/simpletetris/TetrisView.java
+++ SimpleTetris/src/org/example/simpletetris/TetrisView.java
@@ -7,81 +7,99 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class TetrisView extends View {
-	
+
 	// TODO: temporary!!!!
-	private final Random _random = new Random(System.currentTimeMillis());
 	private static final int _ROWS = 20;
 	private static final int _COLUMNS = 10;
 	private final int[][] _data = new int[_ROWS][_COLUMNS];
 	private static final int[] _COLORS = { Color.WHITE, 0xff008040, 0xff804000,
-		0xff000080, 0xff0080cc, 0xffcc0040, 0xffcccc00 };
-	
-	
-	private static final int BLOCK_OFFSET = 2;		
-	private static final int FRAME_OFFSET = 10;	
-	
+			0xff000080, 0xff0080cc, 0xffcc0040, 0xffcccc00, Color.WHITE,
+			Color.WHITE, Color.WHITE, };
+	private RedrawHandler _redrawHandler = new RedrawHandler();
+
+	private static final int BLOCK_OFFSET = 2;
+	private static final int FRAME_OFFSET = 10;
+
 	private final Paint paint = new Paint();
-	
+
 	private int width;
 	private int height;
 	private int cellWidth;
 	private int cellHeight;
-	
+
 	public TetrisView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		init();
+		update();
 	}
+
 	public TetrisView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		init();
+		update();
 	}
-	
-	private void init() {
-		Random  random = new Random( System.currentTimeMillis() );
-		for( int y=0; y<_data.length; y++ ) {
-			for( int x=0; x<_data[y].length; x++ ) {
+
+	private void update() {
+		Random random = new Random(System.currentTimeMillis());
+		for (int y = 0; y < _data.length; y++) {
+			for (int x = 0; x < _data[y].length; x++) {
 				int n = random.nextInt(_COLORS.length);
 				_data[y][x] = n;
 			}
 		}
+		_redrawHandler.sleep(2000);
 	}
-	
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		paint.setColor( Color.GRAY );
-		canvas.drawRect( 0, 0, width, height, paint);
-		paint.setColor( Color.WHITE );
-		canvas.drawRect( FRAME_OFFSET, FRAME_OFFSET, width - FRAME_OFFSET, height - FRAME_OFFSET, paint);
-		
-		for( int y=0; y<_data.length; y++ ) {
-			for( int x=0; x<_data[y].length; x++ ) {
+		paint.setColor(Color.GRAY);
+		canvas.drawRect(0, 0, width, height, paint);
+		paint.setColor(Color.WHITE);
+		canvas.drawRect(FRAME_OFFSET, FRAME_OFFSET, width - FRAME_OFFSET,
+				height - FRAME_OFFSET, paint);
+
+		for (int y = 0; y < _data.length; y++) {
+			for (int x = 0; x < _data[y].length; x++) {
 				// Draw cell:
-				int color = _COLORS[ _data[y][x] ];
+				int color = _COLORS[_data[y][x]];
 				paint.setColor(color);
 				float left = FRAME_OFFSET + cellWidth * x + BLOCK_OFFSET;
 				float top = FRAME_OFFSET + cellHeight * y + BLOCK_OFFSET;
 				float right = FRAME_OFFSET + cellWidth * (x + 1) - BLOCK_OFFSET;
-				float bottom = FRAME_OFFSET + cellHeight * (y + 1) - BLOCK_OFFSET;
-				RectF rect = new RectF( left, top, right, bottom);
+				float bottom = FRAME_OFFSET + cellHeight * (y + 1)
+						- BLOCK_OFFSET;
+				RectF rect = new RectF(left, top, right, bottom);
 				canvas.drawRoundRect(rect, 4, 4, paint);
 			}
 		}
-		
-	}	
-	
-	// TODO: reset cell size
+
+	}
+
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
 		width = w;
 		height = h;
-		cellWidth = (width - 2 * FRAME_OFFSET ) / _COLUMNS;
-		cellHeight = (height - 2 * FRAME_OFFSET ) / _ROWS;
-	}	
+		cellWidth = (width - 2 * FRAME_OFFSET) / _COLUMNS;
+		cellHeight = (height - 2 * FRAME_OFFSET) / _ROWS;
+	}
+	
+	private class RedrawHandler extends Handler {
 
+		@Override
+		public void handleMessage(Message msg) {
+			TetrisView.this.update();
+			TetrisView.this.invalidate();
+		}
+
+		public void sleep(long delayMillis) {
+			this.removeMessages(0);
+			sendMessageDelayed(obtainMessage(0), delayMillis);
+		}
+	};
 }
