f3584c384d2211b4cbe6e0839e1494df0ca411fb
OleksandrPavloshchuk <ap730518@gmail.com>
2013-08-24 19:46:51 +0300

    simple gradient background

--- SimpleTetris/src/org/example/simpletetris/TetrisView.java
+++ SimpleTetris/src/org/example/simpletetris/TetrisView.java
@@ -9,7 +9,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.Handler;
 import android.os.Message;
